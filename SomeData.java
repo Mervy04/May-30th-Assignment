4641800fa59cd4f88e8b39f7c945e8705b14e2e8
Mervy04 <87144944+Mervy04@users.noreply.github.com>
2022-06-01 07:38:14 -0400

    Update SomeData.java

--- SomeData.java
+++ SomeData.java
@@ -1,3 +1,4 @@
+/*
 package com.company;
 // importing sub classes
 import static com.company.ProjectConstants.*;
@@ -49,3 +50,53 @@ public class SomeData {
     }
 
 }
+*/
+    
+    package com.company;
+// importing sub classes
+import static com.company.Main.*;
+public class SomeData {
+
+    // Data or State Information
+
+    private String userStringData;
+    private int userintData;
+
+    /*
+    // Fields--------------------------------------
+    private double[] datavalues = new double[MAXDATA];
+     */
+
+
+    // public description methods
+/*
+    public void Program_Description(){
+        System.out.println("This Program takes your data and stores in an array to then be printed out");
+    };
+    // Access method(s)
+ */
+
+    public int getUserintData() {
+        return userintData;
+    }
+
+    public int setUserintData(int userintData) {
+        this.userintData = userintData;
+        return userintData;
+    }
+
+    public String getUserStringData() {
+        return userStringData;
+    }
+
+    public void setUserStringData(String userStringData) {
+        this.userStringData = userStringData;
+    }
+
+    public void display(){
+        System.out.println("User First Data:" + );
+
+    }
+
+}
+
